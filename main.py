087e1763251013eaf954e231619ceef012a7f6f3
GOKUL SUBRAMANIAN <96585296+gokul-exe@users.noreply.github.com>
2023-08-16 14:25:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Auto Meme Creator
 # Developed by [Gokul]
-# Version: 1.0
+# Version: 1.1
 
 
 
@@ -8,16 +8,21 @@ from singleimage import singleproc
 from doubleimage import doublproc
 from splitvideo  import crop_and_split_video
 from rename import rename_files
+from video  import vdeoproc,about
 
 def main():
     opt = welcome()
-    if opt == "1":
-        
+    if opt == "1":                
         singleproc()
     elif opt =="2":
         doublproc()
     elif opt=="3":
         crop_and_split_video()
+    elif opt=="4":
+        about()
+        vdeoproc()
+    else: print("WRONG CHOICE RUN AGAIN  ")
+
 
 def welcome():
     print("""███    ███ ███████ ███    ███ ███████      ██████  ███████ ███    ██ ███████ ██████   █████  ████████  ██████  ██████  
@@ -38,7 +43,8 @@ def welcome():
     option = input("""ENTER THE OPTION: 
                    TYPE-1: SINGLE IMAGE MEME
                    TYPE-2: DOUBLE IMAGE MEME
-                   TYPE-3: VIDEO SPLITTER   
+                   TYPE-3: VIDEO SPLITTER 
+                   TYPE-4: IMAGE AND VIDEO COLLAGE  
                    ENTER YOUR CHOICE :  """)
     return option
 
